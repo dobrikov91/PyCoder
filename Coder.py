39c9da4c79357a997246586b9385d5980cb58a12
Te4NIK <Te4NIK1@gmail.com>
2012-11-19 15:14:20 +0400

    Test commit

--- Coder.py
+++ Coder.py
@@ -5,14 +5,22 @@ import sys
 print "pycoder"
 
 def main(argv=sys.argv):
-	if len(sys.argv) == 3:
-		s="ffmpeg -i "+sys.argv[1]+" -f mp4 -acodec libfaac -vcodec libx264 -threads 2 -y "+sys.argv[2]
+	if len(sys.argv) == 1 or sys.argv[1] == "-h" or sys.argv[1] == "--help":
+		print "Enter the two parameters - the names of the input and output files"
+		print "sample - Pycoder.py In_file.ogg Out_file.mp4"
+		print ""
+		print "You can also make a piece of video encoding"
+		print "To do this, after the main parameters to specify the start time and the duration of the fragment"
+		print "sample - Pycoder.py In_file.ogg Out_file.mp4 00:01:15 00:00:20"
 	else:
-		s="ffmpeg -i "+sys.argv[1]+" -f mp4 -acodec libfaac -vcodec libx264 -ss "+sys.argv[3]+" -t "+sys.argv[4]+" -threads 2 -y "+sys.argv[2]	
-
-	t = subprocess.Popen(s.split(' '),stderr=subprocess.STDOUT,stdout = subprocess.PIPE)
-	out = t.communicate()
-	print out
+		if len(sys.argv) == 3:
+			s="ffmpeg$-i$"+sys.argv[1]+"$-f$mp4$-acodec$libfaac$-vcodec$libx264$-threads$2$-y$\""+sys.argv[2]+"\""
+		else:
+			s="ffmpeg$-i$"+sys.argv[1]+"$-f$mp4$-acodec$libfaac$-vcodec$libx264$-ss$"+sys.argv[3]+"$-t$"+sys.argv[4]+"$-threads 2$-y$"+sys.argv[2]	
+		print s
+		t = subprocess.Popen(s.split('$'),stderr=subprocess.STDOUT,stdout = subprocess.PIPE)
+		out = t.communicate()
+		print out
 	
 if __name__ == "__main__":
     main()
